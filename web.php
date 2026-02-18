7806e5618524aff06ba6cbcc86187f3f58f17636
mahrshiparmar024-dev <mahrshiparmar024@gmail.com>
2026-02-18 15:57:40 +0530

    Add files via upload

---
+++ web.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+Route::get('/', function () {
+    return redirect('login');
+});
+
+Route::post('getFriends', 'HomeController@getFriends');
+Route::post('/session/create', 'SessionController@create');
+Route::post('/session/{session}/chats', 'ChatController@chats');
+Route::post('/session/{session}/read', 'ChatController@read');
+Route::post('/session/{session}/clear', 'ChatController@clear');
+Route::post('/session/{session}/block', 'BlockController@block');
+Route::post('/session/{session}/unblock', 'BlockController@unblock');
+Route::post('/send/{session}', 'ChatController@send');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/chat', 'HomeController@chat')->name('chat');
+
+Auth::routes();
